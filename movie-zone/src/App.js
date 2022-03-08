0303141a31b28c475976633d417d26d39597e90f
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-08 10:25:41 -0700

    initial commit

---
+++ movie-zone/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeaderBar from "./components/headerBar";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderBar />
+      <h3>Welcome to Movie Zone</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-zone/src/components/headerBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./headerBar.css";
+
+function HeaderBar() {
+  return <div className="header-container">This is my Header</div>;
+}
+
+export default HeaderBar;
