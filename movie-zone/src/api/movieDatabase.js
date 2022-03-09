1b04e281111ab114acdda35c751e2992d6237596
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-09 14:12:38 -0700

    .env file added

--- movie-zone/src/api/movieDatabase.js
+++ movie-zone/src/api/movieDatabase.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const KEY = process.env.REACT_APP_API_KEY;
+
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "42fafab1d67d95a1993da7575e94ff60",
+    api_key: KEY,
     include_adult: false,
   },
 });
