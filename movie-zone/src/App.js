76b11b1e024d652a52a1ad010c5209ada33ea205
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-23 14:17:32 -0600

    routes changed

--- movie-zone/src/App.js
+++ movie-zone/src/App.js
@@ -1,39 +1,22 @@
 import "./App.css";
-import {render} from "react-dom";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import HeaderBar from "./components/headerBar";
-import movieDatabase from "./api/movieDatabase";
-import {useEffect} from "react";
+
 import SearchBar from "./components/searchBar";
 import SingleMovie from "./pages/singleMove";
 
 function App() {
-  useEffect(() => {
-    movieDatabase
-      .get("/movie/271110")
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <HeaderBar />
 
         <SearchBar />
-        <Routes>
+        <Switch>
           <Route exact path="/movie" element={<SingleMovie />} />
-        </Routes>
-      </BrowserRouter>
+        </Switch>
+      </Router>
     </div>
   );
 }
