a542e5c4fefe69c8038208cac867b054372a75df
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-06-30 18:58:54 -0600

    nowplaying card css

--- movie-zone/src/components/nowPlaying.js
+++ movie-zone/src/components/nowPlaying.js
@@ -7,11 +7,13 @@ import movieDatabase from "../api/movieDatabase";
 function NowPlaying() {
   const [nowPlaying, setNowPlaying] = useState([]);
 
+  const imageURL = "https://image.tmdb.org/t/p/w1280";
+
   useEffect(() => {
     movieDatabase
       .get("/movie/now_playing")
       .then((response) => {
-        setNowPlaying(response.data.original_title);
+        setNowPlaying(response.data.results);
         console.log(response.data.results);
       })
       .catch((error) => {
@@ -21,20 +23,26 @@ function NowPlaying() {
 
   return (
     <div className="now-playing-container">
-      <h3>Now Playing</h3>
+      <h3>Movies Now Playing</h3>
 
-      {nowPlaying.map((movie) => {
-        nowPlaying.sort(function (a, b) {
-          return b.id - a.id;
-        });
-        return (
-          <div>
-            <ul>
-              <li>{movie.original_title}</li>
-            </ul>
-          </div>
-        );
-      })}
+      <div className="card-container">
+        {nowPlaying.map((movie) => {
+          nowPlaying.sort(function (a, b) {
+            return b.id - a.id;
+          });
+          return (
+            <div className="top-container">
+              <div className="now-playing-card">
+                <img
+                  src={imageURL + movie.poster_path}
+                  alt={movie.title}
+                  className="image"
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
