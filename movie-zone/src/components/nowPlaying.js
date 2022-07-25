639efad050971ecae18463828553b5d3c6366791
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-07-25 17:03:33 -0600

    now playing component

--- movie-zone/src/components/nowPlaying.js
+++ movie-zone/src/components/nowPlaying.js
@@ -33,39 +33,40 @@ function NowPlaying() {
     <div className="now-playing-container">
       <h3>Movies Now Playing</h3>
 
-      <div className="card-container">
+      {/* <div className="card-container">
         {nowPlaying.map((movie) => {
           nowPlaying.sort(function (a, b) {
             return b.id - a.id;
           });
           return (
-            // <div className="top-container">
-            //   <div className="now-playing-card">
-            //     <img
-            //       src={imageURL + movie.poster_path}
-            //       alt={movie.title}
-            //       className="image"
-            //     />
-            //   </div>
-            // </div>
-
-            <div className="slideshow-container">
-              <div className="slides fade">
+            <div className="top-container">
+              <div className="now-playing-card">
                 <img
                   src={imageURL + movie.poster_path}
                   alt={movie.title}
                   className="image"
                 />
               </div>
-
-              <a className="previous" onClick={nextSlides}>
-                &#10094;
-              </a>
-              <a className="next" onClick={previousSlides}></a>
             </div>
+
+            // <div className="slideshow-container">
+            //   <div className="slides fade">
+            //     <img
+            //       src={imageURL + movie.poster_path}
+            //       alt={movie.title}
+            //       className="image"
+            //     />
+                
+            //   </div>
+
+            //   <a className="previous" onClick={nextSlides}>
+            //     &#10094;
+            //   </a>
+            //   <a className="next" onClick={previousSlides}></a>
+            // </div>
           );
         })}
-      </div>
+      </div> */}
     </div>
   );
 }
