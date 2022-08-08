090772b80ef66ffd55cf2c0be17ac4618d990874
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-08-08 17:02:35 -0600

    recommendations list rendered to screen

--- movie-zone/src/components/recommendations.js
+++ movie-zone/src/components/recommendations.js
@@ -5,30 +5,40 @@ import {useParams} from "react-router-dom";
 
 function MovieRecommendations() {
   const [movieTitle, setMovieTitle] = useState([]);
+
   const {id} = useParams();
+
+  const imageURL = "https://image.tmdb.org/t/p/w200";
 
   useEffect(() => {
     movieDatabase
       .get(`movie/${id}/recommendations`)
       .then((response) => {
         console.log(response.data.results);
-        setMovieTitle(response.data.results.title);
+        setMovieTitle(response.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
-  });
-
-  console.log(movieTitle);
+  }, []);
 
   return (
     <div>
       <h1 className="title">Movie Recs</h1>
 
-      <div>
-        <ol>
-          <li>{movieTitle}</li>
-        </ol>
+      <div className="rec-image-container">
+        {movieTitle.map((rec) => {
+          movieTitle.sort(function (a, b) {
+            return b.id - a.id;
+          });
+
+          return (
+            <div className="rec-movies-list">
+              {/* <li>{rec.title}</li> */}
+              <img src={imageURL + rec.poster_path} alt={rec.title} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
